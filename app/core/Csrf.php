3660c928266c78cd1d4e863debb55e93c1a1c954
Sebastian_rodriguez <sebastian.rodriguezc@cecar.edu.co>
2026-06-12 19:22:45 -0500

    cores

---
+++ app/core/Csrf.php
@@ -0,0 +1,19 @@
+<?php
+
+class Csrf
+{
+    public static function token(): string
+    {
+        if (empty($_SESSION['_csrf'])) {
+            $_SESSION['_csrf'] = bin2hex(random_bytes(16));
+        }
+
+        return $_SESSION['_csrf'];
+    }
+
+    public static function validate(?string $token): bool
+    {
+        return is_string($token) && isset($_SESSION['_csrf']) && hash_equals($_SESSION['_csrf'], $token);
+    }
+}
+
